a29dd09fe88e6258785c685fcfd310b85c20da71
drewp <drewp@bigasterisk.com>
2010-03-20 22:09:41 -0700

    distro stuff
    Ignore-this: f913747ebcb51f34d0b8628d88b2347a
    
    darcs-hash:20100321050941-312f9-bd63d0d2bd897eeff44eedf344350bd7038d2be3.gz

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from distutils.core import setup
 
 setup(name="sparqlhttp",
-      version="1.3",
+      version="1.4",
       description="HTTP SPARQL server and client for twisted and rdflib",
       author="Drew Perttula",
       author_email="drewp@bigasterisk.com",
       url="http://projects.bigasterisk.com/sparqlhttp/",
-      download_url="http://projects.bigasterisk.com/sparqlhttp-1.3.tar.gz",
+      download_url="http://projects.bigasterisk.com/sparqlhttp-1.4.tar.gz",
 
       packages=['sparqlhttp'],
       py_modules=['test.shared'],
