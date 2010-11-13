cd7e8c105a0426266cf4a824fdc77bd24360c240
drewp <drewp@bigasterisk.com>
2010-11-12 18:36:08 -0800

    turn off setuptools_trial
    Ignore-this: b6946147f871f7461273696e030ee7a9
    
    darcs-hash:20101113023608-312f9-eaf0dc5c1e9ea17beba0766f9004115e1f706c35.gz

--- setup.py
+++ setup.py
@@ -3,14 +3,15 @@
 from setuptools import setup
 
 setup(name="sparqlhttp",
-      version="1.10",
+      version="1.11",
       description="HTTP SPARQL server and client for twisted and rdflib",
       author="Drew Perttula",
       author_email="drewp@bigasterisk.com",
       url="http://projects.bigasterisk.com/sparqlhttp/",
-      download_url="http://projects.bigasterisk.com/sparqlhttp/sparqlhttp-1.10.tar.gz",
+      download_url="http://projects.bigasterisk.com/sparqlhttp/sparqlhttp-1.11.tar.gz",
 
-      setup_requires=['setuptools_trial >= 0.5'],
+      # this seems to be making other projects break, especially with buildout
+      #setup_requires=['setuptools_trial >= 0.5'],
 
       packages=['sparqlhttp'],
       data_files=[('', ['doc.html'])],
