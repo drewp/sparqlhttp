85c2445f689771a8fd862ccfe25830525b0d9c5a
drewp <drewp@bigasterisk.com>
2012-01-12 22:17:15 -0800

    new release 1.13
    Ignore-this: 70f107d0880f9b0101070e3357931de3
    
    darcs-hash:20120113061715-312f9-10a8bcb5a7547a24b44d56d03a87b68dced7e812.gz

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 from setuptools import setup
 
 setup(name="sparqlhttp",
-      version="1.12",
+      version="1.13",
       description="HTTP SPARQL server and client for twisted and rdflib",
       author="Drew Perttula",
       author_email="drewp@bigasterisk.com",
       url="http://projects.bigasterisk.com/sparqlhttp/",
-      download_url="http://projects.bigasterisk.com/sparqlhttp/sparqlhttp-1.12.tar.gz",
+      download_url="http://projects.bigasterisk.com/sparqlhttp/sparqlhttp-1.13.tar.gz",
 
       # this seems to be making other projects break, especially with buildout
       #setup_requires=['setuptools_trial >= 0.5'],
